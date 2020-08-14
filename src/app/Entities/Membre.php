b585fb26194f58f7ae2694ab48b3d2ca1e1f4561
bendahmane kader <38891568+kadkarl@users.noreply.github.com>
2020-08-14 22:07:44 +0200

    Projet doranco

--- src/app/Entities/Membre.php
+++ src/app/Entities/Membre.php
@@ -73,66 +73,6 @@ class Membre
     }
 
     /**
-     * Get the value of nom
-     */ 
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    /**
-     * Set the value of nom
-     *
-     * @return  self
-     */ 
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of prenom
-     */ 
-    public function getPrenom()
-    {
-        return $this->prenom;
-    }
-
-    /**
-     * Set the value of prenom
-     *
-     * @return  self
-     */ 
-    public function setPrenom($prenom)
-    {
-        $this->prenom = $prenom;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of telephone
-     */ 
-    public function getTelephone()
-    {
-        return $this->telephone;
-    }
-
-    /**
-     * Set the value of telephone
-     *
-     * @return  self
-     */ 
-    public function setTelephone($telephone)
-    {
-        $this->telephone = $telephone;
-
-        return $this;
-    }
-
-    /**
      * Get the value of email
      */ 
     public function getEmail()
